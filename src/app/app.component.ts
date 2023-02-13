276451756893537d88958a5d12f9fe1b037697d1
Vitaliy K <vitaliyk@live.com>
2023-02-13 18:14:25 +0300

    correct figures for PKKB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'counter';
-  /* mainCounter: number = 0; */
+
   currentDate = new Date();
 
-  /* count numbers for ПККБ */
-  PKKBstartDate = new Date("01/31/2023");
+/* count numbers for ПККБ */
+/* 11559 */
+  PKKBstartDate = new Date("12/01/2018");
   PKKBdiff = Math.abs(this.PKKBstartDate.getTime() - this.currentDate.getTime());
   PKKBdiffDays = Math.ceil(this.PKKBdiff / (1000 * 3600 * 24));
-  PKKBpatientsPerDay: number = 5;
+  PKKBpatientsPerDay = (11559 / this.PKKBdiffDays);
   PKKBpatientsTotal = Math.ceil(this.PKKBdiffDays * this.PKKBpatientsPerDay);
 
-  /* count numbers for НМХЦ Пирогова */
+/* count numbers for НМХЦ Пирогова */
   PirogovStartDate = new Date("12/31/2019");
   PirogovDiff = Math.abs(this.PirogovStartDate.getTime() - this.currentDate.getTime());
   PirogovDiffDays = Math.ceil(this.PirogovDiff / (1000 * 3600 * 24));
-  PirogovPatientsPerDay: number = 50;
+  PirogovPatientsPerDay: number = 90;
   PirogovPatientsTotal = Math.ceil(this.PirogovDiffDays * this.PirogovPatientsPerDay);
 
   mainCounter = Math.ceil(this.PKKBpatientsTotal + this.PirogovPatientsTotal)
