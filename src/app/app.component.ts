ed505a53f3c25ed2fdb71261f5770e6c94dbbfe6
Vitaliy K <vitaliyk@live.com>
2023-02-13 13:43:21 +0300

    basic counters logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter';
+  /* mainCounter: number = 0; */
+  currentDate = new Date();
 
-  mainCounter: number = 0;
+  /* count numbers for ПККБ */
+  PKKBstartDate = new Date("01/31/2023");
+  PKKBdiff = Math.abs(this.PKKBstartDate.getTime() - this.currentDate.getTime());
+  PKKBdiffDays = Math.ceil(this.PKKBdiff / (1000 * 3600 * 24));
+  PKKBpatientsPerDay: number = 5;
+  PKKBpatientsTotal = Math.ceil(this.PKKBdiffDays * this.PKKBpatientsPerDay);
+
+  /* count numbers for НМХЦ Пирогова */
+  PirogovStartDate = new Date("12/31/2019");
+  PirogovDiff = Math.abs(this.PirogovStartDate.getTime() - this.currentDate.getTime());
+  PirogovDiffDays = Math.ceil(this.PirogovDiff / (1000 * 3600 * 24));
+  PirogovPatientsPerDay: number = 50;
+  PirogovPatientsTotal = Math.ceil(this.PirogovDiffDays * this.PirogovPatientsPerDay);
+
+  mainCounter = Math.ceil(this.PKKBpatientsTotal + this.PirogovPatientsTotal)
 
   increment() {
     this.mainCounter++;
     return this.mainCounter;
   }
+
 }
